951f89008b256735c57b8a1e0281895ee6a673d3
CreakyCoss <1085892854@qq.com>
2018-03-03 23:33:41 +0800

    性能

--- app/main/views.py
+++ app/main/views.py
@@ -6,6 +6,18 @@ from .. import db
 from ..models import User, Role, Permission, Post, Comment
 from ..decorators import admin_required, permission_required
 from flask_login import login_required, current_user
+from flask_sqlalchemy import get_debug_queries
+
+
+@main.after_app_request
+def after_request(response):
+    for query in get_debug_queries():
+        if query.duration >= current_app.config['FLASKY_SLOW_DB_QUERY_TIME']:
+            current_app.logger.warning(
+                'Slow query: %s\nParameters: %s\nDuration: %fs\nContext: %s\n'
+                % (query.statement, query.parameters, query.duration,
+                   query.context))
+    return response
 
 
 @main.route('/', methods=['GET', 'POST'])
